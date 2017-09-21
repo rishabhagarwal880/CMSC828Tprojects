487f8694fe05fd252098af9749f20d594eaea3fb
Rishabh Agarwal <rishabh@wireless-10-104-232-217.umd.edu>
2017-09-21 15:37:35 -0400

    Project1 phase2 completed

--- P1Ph2/controller.m
+++ P1Ph2/controller.m
@@ -13,11 +13,45 @@ phi_des = 0;
 theta_des = 0;
 psi_des = 0;
 
+roll =qd{1}.euler(1);
+pitch =qd{1}.euler(2);
+yaw =qd{1}.euler(3);
+p_angle = qd{1}.omega(1);
+q_angle =qd{1}.omega(2);
+r_angle =qd{1}.omega(3);
+
+pos_des = qd{1}.pos_des;
+vel_des = qd{1}.vel_des;
+acc_des = qd{1}.acc_des;
+
+pos_e = pos_des-qd{1}.pos;
+vel_e = vel_des-qd{1}.vel;
+acc_e = acc_des;
+
+
+kp = 20;
+kp_ang = 200;
+kd = 5;
+kd_ang = 25;
+
+acc_final = acc_e+kd*vel_e+kp*pos_e;
+
+
+m = params.mass;
+g = params.grav;
+F_des = m*g + m*(acc_final(3));
+
+phi_des = 1/g*(acc_final(1)*sin(yaw)-acc_final(2)*cos(yaw));
+theta_des = 1/g*(acc_final(1)*cos(yaw)+acc_final(2)*sin(yaw));
+
 % Thurst
-F    = 0;
+F = F_des;
+
 
 % Moment
 M    = zeros(3,1); % You should fill this in
+M = params.I*[kp_ang*(phi_des-roll)+kd_ang*(-p_angle); ...
+    kp_ang*(theta_des-pitch)+kd_ang*(-q_angle); kp_ang*(psi_des-yaw)+kd_ang*(-r_angle)];
 % =================== Your code ends here ===================
 
 % Output trpy and drpy as in hardware
--- P1Ph2/trajectories/circle.m
+++ P1Ph2/trajectories/circle.m
@@ -10,6 +10,30 @@ acc = [0; 0; 0];
 yaw = 0;
 yawdot = 0;
 
+T = 15;
+f = .0001;
+curve = 2*pi/((pi^.5*erf(f^.5*(T-T/2))/(2*f^.5)-T*exp(-f*T^2/4))-pi^.5*...
+    erf(-f^.5*T/2)/(2*f^.5));
+c =-curve*pi^.5*erf(f^.5*T/2)/(2*f^.5)+curve*T*exp(-f*T^2/4)+2*pi;
+theta = curve*(pi^.5*erf(f^.5*(t-T/2))/(2*f^.5)-t*exp(-f*T^2/4))+c;
+theta_dot = curve*(exp(-(f^.5*t-f^.5*T/2)^2)-exp(-f*T^2/4));
+theta_ddot = f*curve*(T-2*t)*exp(-.25*f*(T-2*t)^2);
+
+pos = [5*cos(theta); 5*sin(theta); 2.5/(2*pi)*theta];
+vel = [-5*(theta_dot)*sin(theta); 5*(theta_dot)*cos(theta); 2.5/(2*pi)*theta_dot];
+acc = [-5*(theta_dot)^2*cos(theta); -5*(theta_dot)^2*sin(theta); 2.5/(2*pi)*theta_ddot];
+
+
+if (t>=T) 
+
+    pos = [5; 0; 2.5];
+    vel = [0; 0; 0];
+    acc = [0; 0; 0];
+    yaw = 0;
+    yawdot = 0;
+end
+
+
 % =================== Your code ends here ===================
 
 desired_state.pos = pos(:);
