ff6d5c7f48f88f7b306f5ff584a431b1f44f4240
Rishabh Agarwal <rishabh@wireless-10-104-228-29.umd.edu>
2017-10-03 13:53:30 -0400

    organization

--- gtsam_toolbox/+gtsam/+utilities/createKeyVector.m
+++
@@ -1,8 +0,0 @@
-function varargout = createKeyVector(varargin)
-      if length(varargin) == 1 && isa(varargin{1},'double')
-        varargout{1} = gtsam_wrapper(2329, varargin{:});
-      elseif length(varargin) == 2 && isa(varargin{1},'char') && isa(varargin{2},'double')
-        varargout{1} = gtsam_wrapper(2330, varargin{:});
-      else
-        error('Arguments do not match any overload of function gtsam.utilities.createKeyVector');
-      end
